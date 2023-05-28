04eb9f9dcb762811dada0d80ae344b7335eaa688
oshadaera68 <mindartlk010@gmail.com>
2023-05-28 23:12:50 +0530

    bugsre fixed

--- src/main/java/com/devstack/app/dao/impl/CustomerDaoImpl.java
+++ src/main/java/com/devstack/app/dao/impl/CustomerDaoImpl.java
@@ -2,14 +2,15 @@ package com.devstack.app.dao.impl;
 
 import com.devstack.app.dao.CustomerDao;
 import com.devstack.app.entity.Customer;
-import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
+
 /*Component--> (@Controller, Repository)*/
 @Repository
 public class CustomerDaoImpl implements CustomerDao {
